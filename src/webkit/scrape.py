7024dee7ee591012db6f471806b09e78cd9e46dd
ajrlewis <hello@ajrlewis.com>
2024-10-24 10:15:47 +0100

    Removed excess white space from text returned.

--- src/webkit/scrape.py
+++ src/webkit/scrape.py
@@ -2,13 +2,13 @@ import datetime
 import random
 import re
 from typing import Optional
+import urllib.parse
 
 from bs4 import BeautifulSoup
 from bs4.element import Comment, Doctype
 import httpx
 from loguru import logger
 
-import urllib.parse
 
 list_of_headers = [
     {
@@ -39,7 +39,9 @@ def sanitize_url(url: str) -> str:
 
 
 def get_response(url: str) -> Optional[httpx.Response]:
+    logger.debug(f"{url = }")
     headers = random.choice(list_of_headers)
+    logger.debug(f"{headers = }")
     try:
         response = httpx.get(url, headers=headers, follow_redirects=True)
         response.raise_for_status()
@@ -47,6 +49,23 @@ def get_response(url: str) -> Optional[httpx.Response]:
         logger.error(f"{e = }")
     else:
         return response
+
+
+# Return error as well
+# def get_response(url: str) -> Optional[httpx.Response], str:
+#     headers = random.choice(list_of_headers)
+#     try:
+#         response = httpx.get(url, headers=headers, follow_redirects=True)
+#         response.raise_for_status()
+#         return response
+#     except httpx.HTTPStatusError as e:
+#         logger.error(f"Error making request to {url}: {e}")
+#     except httpx.TimeoutException:
+#         logger.error(f"Timeout error making request to {url}")
+#     except httpx.ConnectionError:
+#         logger.error(f"Connection error making request to {url}")
+#     except Exception as e:
+#         logger.error(f"Unknown error making request to {url}: {e}")
 
 
 def tag_visible(element: Doctype) -> bool:
@@ -70,6 +89,7 @@ def text_from_html(markup: str, features: str = "html.parser") -> str:
     visible_texts = filter(tag_visible, texts)
     visible_text = " ".join(visible_text.strip() for visible_text in visible_texts)
     visible_text = visible_text.strip()
+    visible_text = re.sub(" +", " ", visible_text)
     return visible_text
 
 
@@ -77,26 +97,27 @@ def text_from_url(url: str) -> dict:
     logger.debug(f"{url = }")
     sanitized_url = sanitize_url(url)
     logger.debug(f"{sanitized_url = }")
+    data = {
+        "url": url,
+        "sanitized_url": sanitized_url,
+        "redirected_url": "",
+        "text": "",
+        "error": "",
+        "scraped_on": f"{datetime.datetime.now(datetime.UTC)}",
+    }
+    logger.debug(f"{data = }")
     response = get_response(sanitized_url)
     logger.debug(f"{response = }")
-    text = ""
     if response:
         redirected_url = response.url
         logger.debug(f"{redirected_url = }")
         body = response.text
         text = text_from_html(body)
         logger.debug(f"{text = }")
-        return {
-            "url": url,
-            "sanitized_url": sanitized_url,
-            "redirected_url": f"{redirected_url}",
-            "text": text,
-            "scraped_on": f"{datetime.datetime.utcnow()}",
-        }
-    return {
-        "url": url,
-        "sanitized_url": sanitized_url,
-        "redirected_url": f"{redirected_url}",
-        "text": text,
-        "scraped_on": f"{datetime.datetime.utcnow()}",
-    }
+        data["text"] = text
+        data["redirected_url"] = f"{redirected_url}"
+    return data
+
+
+x = text_from_url("ajrlewis.com")
+logger.info(x)
