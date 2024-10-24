7469794fcb72ac65576f4842df4f3dc885d45ee8
ajrlewis <hello@ajrlewis.com>
2024-10-24 15:44:21 +0100

    Added duckduckgo_search search method.

--- src/webkit/search.py
+++ src/webkit/search.py
@@ -1,14 +1,27 @@
 from duckduckgo_search import DDGS
+from duckduckgo_search.exceptions import (
+    DuckDuckGoSearchException,
+    RatelimitException,
+    TimeoutException,
+)
 from loguru import logger
 
 
 def duckduckgo_search(keywords: str, max_results: int = 10) -> list[dict]:
+    logger.debug(f"{keywords = } {max_results = }")
     try:
         results = DDGS().text(keywords, safesearch="off", max_results=max_results)
+        logger.debug(f"{len(results)} results found.")
+    except DuckDuckGoSearchException as e:
+        logger.error(f"DuckDuckGoSearchException: {e}")
+        results = []
+    except RatelimitException as e:
+        logger.error(f"RatelimitException: {e}")
+        results = []
+    except TimeoutException as e:
+        logger.error(f"TimeoutException: {e}")
+        results = []
     except Exception as e:
-        # DuckDuckGoSearchException
-        # RatelimitException
-        # TimeoutException
-        logger.error(f"{e = }")
+        logger.error(f"Unknown error: {e}")
         results = []
     return results
