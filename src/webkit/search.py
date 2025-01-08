f1f6cf7b9ea2de272323eeb19a32bf5bd902c78a
ajrlewis <hello@ajrlewis.com>
2025-01-08 21:26:47 +0000

    Added google_search method

--- src/webkit/search.py
+++ src/webkit/search.py
@@ -1,3 +1,6 @@
+import os
+
+from dotenv import load_dotenv
 from duckduckgo_search import DDGS
 from duckduckgo_search.exceptions import (
     DuckDuckGoSearchException,
@@ -5,6 +8,12 @@ from duckduckgo_search.exceptions import (
     TimeoutException,
 )
 from loguru import logger
+
+from . import scrape
+
+load_dotenv()
+google_search_api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
+google_search_id = os.getenv("GOOGLE_SEARCH_ID")
 
 
 def duckduckgo_search(keywords: str, max_results: int = 10) -> list[dict]:
@@ -25,3 +34,39 @@ def duckduckgo_search(keywords: str, max_results: int = 10) -> list[dict]:
         logger.error(f"Unknown error: {e}")
         results = []
     return results
+
+
+def google_search(query: str, max_results: int = 10) -> list[dict]:
+    """https://developers.google.com/custom-search/v1/overview"""
+    search_results = []
+    if not google_search_api_key or not google_search_id:
+        logg.error(
+            "GOOGLE_SEARCH_API_KEY and/or GOOGLE_SEARCH_ID environmental variable not set."
+        )
+        return search_results
+    url = f"https://customsearch.googleapis.com/customsearch/v1"
+    params = {
+        "cx": google_search_id,
+        "key": google_search_api_key,
+        "q": query,
+        "num": max_results,
+        "start": 1,
+    }
+    response, error = scrape.get_response(url, params=params)
+    if response:
+        logger.info(f"{response = }")
+        try:
+            data = response.json()
+            items = data.get("items", [])
+            keys = ("title", "link", "snippet")
+            for item in items:
+                search_result = {k: v for k, v in item.items() if k in keys}
+                search_result["body"] = search_result.pop("snippet")
+                search_result["href"] = search_result.pop("link")
+                search_results.append(search_result)
+        except Exception as e:
+            return []
+    else:
+        logger.info(f"{error = }")
+    logger.info(f"{search_results = }")
+    return search_results
