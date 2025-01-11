2f0ad4f9160b593b8b44330d64fdbb9dd3020bac
ajrlewis <hello@ajrlewis.com>
2025-01-11 19:11:36 +0000

    Added should_scrape_links to google method. Removed free duckduckgo search method

--- src/webkit/search.py
+++ src/webkit/search.py
@@ -1,12 +1,6 @@
 import os
 
 from dotenv import load_dotenv
-from duckduckgo_search import DDGS
-from duckduckgo_search.exceptions import (
-    DuckDuckGoSearchException,
-    RatelimitException,
-    TimeoutException,
-)
 from loguru import logger
 
 from . import scrape
@@ -15,35 +9,25 @@ load_dotenv()
 google_search_api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
 google_search_id = os.getenv("GOOGLE_SEARCH_ID")
 
-
-def duckduckgo(query: str, max_results: int = 10) -> list[dict]:
-    logger.debug(f"{query = } {max_results = }")
-    try:
-        results = DDGS().text(query, safesearch="off", max_results=max_results)
-        logger.debug(f"{len(results)} results found.")
-    except DuckDuckGoSearchException as e:
-        logger.error(f"DuckDuckGoSearchException: {e}")
-        results = []
-    except RatelimitException as e:
-        logger.error(f"RatelimitException: {e}")
-        results = []
-    except TimeoutException as e:
-        logger.error(f"TimeoutException: {e}")
-        results = []
-    except Exception as e:
-        logger.error(f"Unknown error: {e}")
-        results = []
-    return results
+if not google_search_api_key or not google_search_id:
+    message = (
+        "GOOGLE_SEARCH_API_KEY and/or GOOGLE_SEARCH_ID environmental variable not set."
+    )
+    logger.error(message)
+    raise ValueError(message)
 
 
-def google(query: str, max_results: int = 10) -> list[dict]:
+def google(
+    query: str,
+    max_results: int = 10,
+    page_start: int = 1,
+    should_scrape_links: bool = False,
+) -> list[dict]:
     """https://developers.google.com/custom-search/v1/overview"""
+
     search_results = []
-    if not google_search_api_key or not google_search_id:
-        logg.error(
-            "GOOGLE_SEARCH_API_KEY and/or GOOGLE_SEARCH_ID environmental variable not set."
-        )
-        return search_results
+
+    # Construct search request.
     url = f"https://customsearch.googleapis.com/customsearch/v1"
     params = {
         "cx": google_search_id,
@@ -51,24 +35,36 @@ def google(query: str, max_results: int = 10) -> list[dict]:
         "q": query,
         # "sort": "date",
         "num": max_results,
-        "start": 1,
+        "start": page_start,
     }
     response, error = scrape.get_response(url, params=params)
-    if response:
-        logger.debug(f"{response = }".encode("UTF-8"))
-        try:
-            data = response.json()
-            items = data.get("items", [])
-            keys = ("title", "link", "snippet")
-            for item in items:
-                search_result = {k: v for k, v in item.items() if k in keys}
-                search_result["snippet"] = search_result.pop("snippet")
-                search_result["body"] = ""
-                search_result["href"] = search_result.pop("link")
-                search_results.append(search_result)
-        except Exception as e:
-            return search_results
-    else:
-        logger.debug(f"{error = }".encode("UTF-8"))
+    logger.debug(f"{response = } {error = }".encode("UTF-8"))
+
+    # Format search results keys.
+    try:
+        data = response.json()
+        items = data.get("items", [])
+        keys = ("title", "link", "snippet")
+        for item in items:
+            search_result = {k: v for k, v in item.items() if k in keys}
+            search_result["href"] = search_result.pop("link")
+            search_result["body"] = ""
+            search_results.append(search_result)
+    except Exception as e:
+        return search_results
     logger.debug(f"{search_results = }".encode("UTF-8"))
+
+    # Scrape the search result links to get website body.
+    if should_scrape_links:
+        _search_results = []
+        for search_result in search_results:
+            _search_result = search_result
+            if href := search_result.get("href"):
+                logger.debug(f"{href = }".encode("UTF-8"))
+                data = scrape.data_from_url(url=href)
+                if body := data.get("text"):
+                    _search_result = _search_result | {"body": body}
+            _search_results.append(_search_result)
+        search_results = _search_results
+
     return search_results
