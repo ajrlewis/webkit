ff9cb9470a62c167687fdc9d1e9b4d5f444a71a6
ajrlewis <hello@ajrlewis.com>
2025-05-15 18:39:48 +0100

    Added Brave search

--- src/webkit/search.py
+++ src/webkit/search.py
@@ -6,17 +6,64 @@ from loguru import logger
 
 from . import scrape
 
-# Check the environmental variables are configured.
 load_dotenv()
-google_search_api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
-google_search_id = os.getenv("GOOGLE_SEARCH_ID")
 
-if not google_search_api_key or not google_search_id:
-    message = (
-        "GOOGLE_SEARCH_API_KEY and/or GOOGLE_SEARCH_ID environmental variable not set."
-    )
-    logger.error(message)
-    raise ValueError(message)
+
+def brave(
+    query: str, max_results: int = 20, api_key: Optional[str] = None
+) -> list[dict]:
+    """https://api-dashboard.search.brave.com/app/documentation/"""
+    api_key = api_key or os.getenv("BRAVE_SEARCH_API_KEY")
+    if api_key is None:
+        message = "api_key must be passed or set in BRAVE_SEARCH_API_KEY"
+        logger.error(message)
+        return []
+
+    url = f"https://api.search.brave.com/res/v1/web/search"
+    params = {"q": query, "count": max_results}
+    headers = {"X-Subscription-Token": api_key}
+
+    response, error = scrape.get_response(url, params=params, headers=headers)
+    if error:
+        logger.debug(f"Failed to get response: {error = }")
+        return []
+
+    try:
+        data = response.json()
+    except ValueError as e:
+        logger.error(f"JSON decoding failed: {e}")
+        return []
+
+    raw_search_results = data.get("web", {}).get("results", [])
+    search_results = []
+
+    for raw_search_result in raw_search_results:
+        title = raw_search_result.get("title", "")
+        url = raw_search_result.get("url", "")
+        description = raw_search_result.get("description", "")
+        extra_snippets = " ".join(raw_search_result.get("extra_snippets", []))
+        search_result = {
+            "title": title,
+            "url": url,
+            "description": f"{description} {extra_snippets}",
+        }
+        logger.debug(f"{search_result = }")
+        search_results.append(search_result)
+    logger.debug(f"Total search results retrieved: {len(search_results)}")
+    return search_results
+
+
+# google_search_api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
+# google_search_id = os.getenv("GOOGLE_SEARCH_ID")
+
+# if not google_search_api_key or not google_search_id:
+#     message = (
+#         "GOOGLE_SEARCH_API_KEY and/or GOOGLE_SEARCH_ID environmental variable not set."
+#     )
+#     logger.error(message)
+#     raise ValueError(message)
+
+# brave_search_api_key = os.getenv("BRAVE_SEARCH_API_KEY")
 
 
 def google(
@@ -24,7 +71,6 @@ def google(
     max_results: int = 10,
     page_start: int = 1,
     sort_by: Optional[str] = None,
-    should_scrape_links: bool = False,
 ) -> list[dict]:
     """https://developers.google.com/custom-search/v1/overview"""
 
@@ -57,15 +103,5 @@ def google(
     except Exception as e:
         return search_results
     logger.debug(f"{search_results = }".encode("UTF-8"))
-
-    # # Scrape the search result links to get website body.
-    # if should_scrape_links:
-    #     _search_results = []
-    #     for search_result in search_results:
-    #         _search_result = search_result
-    #         if data := scrape.data_from_search_result(search_result):
-    #             _search_result = _search_result | {"body": body}
-    #         _search_results.append(_search_result)
-    #     search_results = _search_results
 
     return search_results
